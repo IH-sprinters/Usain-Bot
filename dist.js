5ed4de0b5641945f64bfaf11455acdb848d8ebb9
Alex Jeffrey <ajeffrey@users.noreply.github.com>
2017-09-11 13:28:05 +0100

    fix issues in checkup, compiled

--- dist.js
+++ dist.js
@@ -310,9 +310,9 @@ module.exports = (hook) => {
     for(const commitment of commitments) {
       const deadline = commitment.deadline && (new Date(commitment.deadline)).getTime();
       const now = Date.now();
-      const created = Sugar.Date.create(commitment.created);
+      const created = Sugar.Date(commitment.created);
       if(deadline && deadline <= now && (now - deadline < 86400000) && !('posted' in commitment)) {
-        post(`on ${created.medium()}, @${commitment.user_name} committed to ${commitment.message} by ${deadline.medium()} - how'd it go?`);
+        post(`on ${created.medium()}, @${commitment.user_name} committed to ${commitment.message} by ${Sugar.Date(deadline).medium()} - how'd it go?`);
         commitment.posted = true;
       }
     }
